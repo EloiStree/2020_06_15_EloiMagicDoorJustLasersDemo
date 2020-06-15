ac7738c3ab37a634f2b904f0b73c7e5b3e845c69
Eloi Stree <eloistree@gmail.com>
2020-06-15 23:58:34 +0200

    Save: 06/15/2020 11:58 PM

---
+++ Runtime/QAD_ChangeColorOnTouched.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QAD_ChangeColorOnTouched : MonoBehaviour
+{
+    public Renderer m_toAffect;
+    public Color m_idltColor = new Color(1f,0,0);
+    public Color m_touchColor = new Color(0.8f, 0, 0);
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        SetAsTouched();
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        SetAsIdlt();
+    }
+
+    public void SetAsIdlt()
+    {
+        m_toAffect.material.color = m_idltColor;
+    }
+    public void SetAsTouched()
+    {
+        m_toAffect.material.color = m_touchColor;
+    }
+
+}
---
+++ Runtime/QAD_LaserSwithLightAlarm.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QAD_LaserSwithLightAlarm : MonoBehaviour
+{
+    public Light m_affectedLight;
+
+    public void SetAlarmFor(float seconds)
+    {
+        m_affectedLight.enabled = true;
+        Invoke("SetAlarmOff", seconds);
+    }
+    public void SetAlarmOff() {
+        m_affectedLight.enabled = false;
+
+    }
+
+
+
+}
