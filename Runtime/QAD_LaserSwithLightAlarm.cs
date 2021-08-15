2db12e8cfef0a5489e7da7eda4898d472816ef13
Éloi Strée <eloistree@gmail.com>
2021-08-15 16:51:35 +0200

    Flush

--- Runtime/QAD_LaserSwithLightAlarm.cs
+++ Runtime/QAD_LaserSwithLightAlarm.cs
@@ -7,17 +7,31 @@ public class QAD_LaserSwithLightAlarm : MonoBehaviour
 {
     public UnityEvent m_startAlarm;
     public UnityEvent m_stopAlarm;
-
+    private float m_countDownState = 3f;
     public void SetAlarmFor(float seconds)
     {
+        m_countDownState = seconds;
         m_startAlarm.Invoke();
-        Invoke("SetAlarmOff", seconds);
     }
     public void SetAlarmOff() {
         m_stopAlarm.Invoke();
 
     }
 
+
+    private void Update()
+    {
+        if ( m_countDownState <= 0f ) 
+            return;
+
+        float previous = m_countDownState;
+        m_countDownState -= Time.deltaTime;
+
+        if (previous > 0f && m_countDownState <= 0f) {
+            m_countDownState = 0f; 
+            SetAlarmOff();
+        }
+    }
 
 
 }
